87f203daad056f311a6265918b1493a6ab6e0bec
Dany99486 <117843005+Dany99486@users.noreply.github.com>
2023-11-12 23:16:50 +0000

    Heartbeat TIMEOUT

--- serverBackup/src/pt/isec/PD/Model/HeartbeatReceiver.java
+++ serverBackup/src/pt/isec/PD/Model/HeartbeatReceiver.java
@@ -6,8 +6,11 @@ import java.io.ObjectInputStream;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
+import java.net.SocketTimeoutException;
 
 public class HeartbeatReceiver extends Thread {
+    private static final int TIMEOUT_SECONDS = 30;
+
     public void run() {
         System.out.println("Heartbeat Receiver started");
         try {
@@ -18,11 +21,19 @@ public class HeartbeatReceiver extends Thread {
             MulticastSocket socket = new MulticastSocket(port);
             socket.joinGroup(group);
 
+            socket.setSoTimeout(TIMEOUT_SECONDS * 1000);
+
+
             while (true) {
                 byte[] buffer = new byte[1024];
                 DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
 
-                socket.receive(packet);
+                try {
+                    socket.receive(packet);
+                } catch (SocketTimeoutException e) {
+                    System.out.println("No heartbeat received for " + TIMEOUT_SECONDS + " seconds. \nExiting...");
+                    System.exit(0);
+                }
 
                 ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(packet.getData());
                 try (ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream)) {
